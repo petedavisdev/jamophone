ebdc063fd624ea5e1ffc3d6fe1cf4b9ffabcb7bf
Pete Davis <Pete.Davis@rcn.org.uk>
2022-12-05 15:05:19 +0000

    onMouseDown={play}

--- src/components/Jar/Jar.tsx
+++ src/components/Jar/Jar.tsx
@@ -15,9 +15,9 @@ export function Jar(props: JarProps): JSX.Element {
 	}
 
 	return (
-		<span className={styles.Jar}>
+		<button className={styles.Jar}>
 			<audio src={'audio/' + props.note + '.mp3'} ref={audio} />
-			<img onMouseOver={play} src={props.image || 'images/jam-strawberry.jpg'} />
-		</span>
+			<img onMouseDown={play} src={props.image || 'images/jam-strawberry.jpg'} />
+		</button>
 	);
 }
