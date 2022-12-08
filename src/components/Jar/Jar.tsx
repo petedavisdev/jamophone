a8b7596618dfccf7c2ec50564ed3a4ac1d39ce5a
petedavisdev <oldmanplanet@gmail.com>
2022-12-08 16:55:35 +0000

    Jar type fix

--- src/components/Jar/Jar.tsx
+++ src/components/Jar/Jar.tsx
@@ -1,6 +1,5 @@
-import { useRef, useState } from 'react';
-
 import styles from './Jar.module.css';
+import { useRef } from 'react';
 
 type JarProps = {
 	note: string;
@@ -9,28 +8,17 @@ type JarProps = {
 };
 
 export function Jar(props: JarProps): JSX.Element {
-	const track1 = useRef(null);
-	const track2 = useRef(null);
-	const track3 = useRef(null);
-	const [nextTrack, setNextTrack] = useState(1);
+	const track = useRef<null | HTMLAudioElement>(null);
 
 	function play() {
 		props.played(props.note);
-		if (nextTrack === 1) {
-			const audioEl = track1.current as null | HTMLAudioElement;
-			audioEl?.play();
-			setNextTrack(2);
-		} else {
-			const audioEl = track2.current as null | HTMLAudioElement;
-			audioEl?.play();
-			setNextTrack(1);
-		}
+		const audioEl = track.current;
+		audioEl?.play();
 	}
 
 	return (
 		<button className={styles.Jar}>
-			<audio src={'/audio/' + props.note + '.mp3'} ref={track1} />
-			<audio src={'/audio/' + props.note + '.mp3'} ref={track2} />
+			<audio src={'/audio/' + props.note + '.mp3'} ref={track} />
 			<audio src={'/audio/' + props.note + '.mp3'} id={props.note} />
 			<img onClick={play} src={props.image || '/images/jam-strawberry.jpg'} />
 		</button>
